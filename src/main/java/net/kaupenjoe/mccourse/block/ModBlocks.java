01fec3f4ecf8c2c6d9504e7c7c322e013b539c2f
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-18 14:40:11 +0100

    stairs + slab

--- src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
+++ src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
@@ -9,6 +9,8 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -41,6 +43,15 @@ public class ModBlocks {
     public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block",
             () -> new SpeedyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB, "tooltip.block.speedy_block");
+
+
+    public static final RegistryObject<Block> COBALT_STAIRS = registerBlock("cobalt_stairs",
+            () -> new StairBlock(() -> ModBlocks.COBALT_BLOCK.get().defaultBlockState(),
+                    BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+    public static final RegistryObject<Block> COBALT_SLAB = registerBlock("cobalt_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
