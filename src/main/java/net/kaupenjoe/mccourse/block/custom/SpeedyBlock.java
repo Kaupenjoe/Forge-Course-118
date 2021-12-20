bfcb9e6c5e026eb2e53d4beafc695873b719f02f
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-20 20:28:32 +0100

    particles

--- src/main/java/net/kaupenjoe/mccourse/block/custom/SpeedyBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/SpeedyBlock.java
@@ -1,6 +1,7 @@
 package net.kaupenjoe.mccourse.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -9,9 +10,40 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.Random;
+
 public class SpeedyBlock extends Block {
     public SpeedyBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, Random pRandom) {
+        float chance = 0.35f;
+
+        if(chance < pRandom.nextFloat()) {
+            pLevel.addParticle(ParticleTypes.SMOKE, pPos.getX() + pRandom.nextDouble(),
+                    pPos.getY() + 0.5D, pPos.getZ() + pRandom.nextDouble(),
+                    0d, 0.015d + pRandom.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < pRandom.nextFloat()) {
+            pLevel.addParticle(ParticleTypes.SMOKE, pPos.getX() + pRandom.nextDouble(),
+                    pPos.getY() + 0.5D, pPos.getZ() + pRandom.nextDouble(),
+                    0d, 0.015d + pRandom.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < pRandom.nextFloat()) {
+            pLevel.addParticle(ParticleTypes.SMOKE, pPos.getX() + pRandom.nextDouble(),
+                    pPos.getY() + 0.5D, pPos.getZ() + pRandom.nextDouble(),
+                    0d, 0.015d + pRandom.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < pRandom.nextFloat()) {
+            pLevel.addParticle(ParticleTypes.SMOKE, pPos.getX() + pRandom.nextDouble(),
+                    pPos.getY() + 0.5D, pPos.getZ() + pRandom.nextDouble(),
+                    0d, 0.015d + pRandom.nextDouble(0.075d), 0d);
+        }
     }
 
     @Override
