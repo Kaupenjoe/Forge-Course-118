dbfe31551878e2ae0386097266b6c0725fe76f65
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-23 12:56:13 +0100

    Global Loot Modifiers

---
+++ src/main/java/net/kaupenjoe/mccourse/event/ModEventBusEvents.java
@@ -0,0 +1,26 @@
+package net.kaupenjoe.mccourse.event;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.event.loot.DowsingRodInIglooAdditionModifier;
+import net.kaupenjoe.mccourse.event.loot.TurnipSeedsFromGrassAdditionModifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nonnull;
+
+@Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+    @SubscribeEvent
+    public static void registerModifierSerializers(@Nonnull final RegistryEvent.Register<GlobalLootModifierSerializer<?>>
+                                                           event) {
+        event.getRegistry().registerAll(
+                new TurnipSeedsFromGrassAdditionModifier.Serializer().setRegistryName
+                        (new ResourceLocation(MCCourseMod.MOD_ID,"turnip_seeds_from_grass")),
+                new DowsingRodInIglooAdditionModifier.Serializer().setRegistryName
+                        (new ResourceLocation(MCCourseMod.MOD_ID,"dowsing_rod_in_igloo"))
+        );
+    }
+}
