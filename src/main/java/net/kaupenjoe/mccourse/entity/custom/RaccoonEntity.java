f64a441ad63bdba7a267ab6c7eeab196c0c1d5ba
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-02-18 15:44:41 +0100

    custom animations

--- src/main/java/net/kaupenjoe/mccourse/entity/custom/RaccoonEntity.java
+++ src/main/java/net/kaupenjoe/mccourse/entity/custom/RaccoonEntity.java
@@ -17,6 +17,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
@@ -76,15 +80,24 @@ public class RaccoonEntity extends Animal implements IAnimatable {
 
     /* ANIMATIONS */
 
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        if (event.isMoving()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.raccoon.walk", true));
+            return PlayState.CONTINUE;
+        }
+
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.raccoon.idle", true));
+        return PlayState.CONTINUE;
+    }
+
     @Override
     public void registerControllers(AnimationData data) {
-
+        data.addAnimationController(new AnimationController(this, "controller",
+                0, this::predicate));
     }
 
     @Override
     public AnimationFactory getFactory() {
         return factory;
     }
-
-
 }
