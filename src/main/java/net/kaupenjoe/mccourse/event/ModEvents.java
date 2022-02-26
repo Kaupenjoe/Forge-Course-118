6b1140fa286af1124ba2a2d0577e14f98ebc7a92
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-02-26 20:37:28 +0100

    custom trades

--- src/main/java/net/kaupenjoe/mccourse/event/ModEvents.java
+++ src/main/java/net/kaupenjoe/mccourse/event/ModEvents.java
@@ -1,22 +1,31 @@
 package net.kaupenjoe.mccourse.event;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.kaupenjoe.mccourse.MCCourseMod;
 import net.kaupenjoe.mccourse.command.ReturnHomeCommand;
 import net.kaupenjoe.mccourse.command.SetHomeCommand;
 import net.kaupenjoe.mccourse.config.MCCourseClientConfigs;
+import net.kaupenjoe.mccourse.item.ModItems;
 import net.kaupenjoe.mccourse.util.KaupenTitleScreen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID)
 public class ModEvents {
@@ -55,6 +64,29 @@ public class ModEvents {
         if (MCCourseClientConfigs.CUSTOM_TITLE_SCREEN.get() &&
                 event.getScreen() instanceof TitleScreen && !(event.getScreen() instanceof KaupenTitleScreen)) {
             event.setScreen(new KaupenTitleScreen());
+        }
+    }
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.TURNIP.get(), 12);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    stack,10,3,0.02F));
+        }
+
+        if(event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.COBALT_PAXEL.get(), 1);
+            int villagerLevel = 3;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 12),
+                    stack,4,5,0.09F));
         }
     }
 }
