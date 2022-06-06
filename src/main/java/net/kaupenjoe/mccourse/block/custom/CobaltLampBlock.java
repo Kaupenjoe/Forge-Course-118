a95589d40c14827c01cb95a3723dced5860dc8ac
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-06 12:49:45 +0200

    redstone block

--- src/main/java/net/kaupenjoe/mccourse/block/custom/CobaltLampBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/CobaltLampBlock.java
@@ -1,6 +1,7 @@
 package net.kaupenjoe.mccourse.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -10,6 +11,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.Random;
 
 public class CobaltLampBlock extends Block {
     public static final BooleanProperty CLICKED = BooleanProperty.create("clicked");
@@ -23,11 +26,33 @@ public class CobaltLampBlock extends Block {
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
                                  Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         if(!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
-            boolean currentState = pState.getValue(CLICKED);
-            pLevel.setBlock(pPos, pState.setValue(CLICKED, !currentState), 3);
+            System.out.println("POWERED BY " + pLevel.getBestNeighborSignal(pPos));
+            // boolean currentState = pState.getValue(CLICKED);
+            // pLevel.setBlock(pPos, pState.setValue(CLICKED, !currentState), 3);
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
+        if (!pLevel.isClientSide) {
+            boolean flag = pState.getValue(CLICKED);
+            if (flag != pLevel.hasNeighborSignal(pPos)) {
+                if (flag) {
+                    pLevel.scheduleTick(pPos, this, 4);
+                } else {
+                    pLevel.setBlock(pPos, pState.cycle(CLICKED), 2);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, Random pRand) {
+        if (pState.getValue(CLICKED) && !pLevel.hasNeighborSignal(pPos)) {
+            pLevel.setBlock(pPos, pState.cycle(CLICKED), 2);
+        }
     }
 
     @Override
